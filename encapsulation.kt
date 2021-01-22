d642ba58c7ba7ca57321c48518700e491e79fb5b
Enes Erdemirci <theazby@hotmail.com>
2021-01-22 16:42:54 +0300

    Update encapsulation.kt

--- encapsulation.kt
+++ encapsulation.kt
@@ -1,5 +1,15 @@
 fun main() {
-  class User{
+
+var user = User()
+//user.name = "Enes" Error: cannot assign to 'name': the setter is private in 'User
+user.lastName = "Erdemirci"
+ 
+println(user.name) // 'null' return
+println(user.lastName)
+ 
+}
+
+class User{
   var uname : String? = null
   var ulastName : String? = null
   var usalary : Int? = null
@@ -12,14 +22,4 @@ fun main() {
   var lastName : String? = ulastName
   var salary : Int? = usalary
   
-}
-
-var user = User()
-//user.name = "Enes" Error: cannot assign to 'name': the setter is private in 'User
-user.lastName = "Erdemirci"
- 
-println(user.name) // 'null' return
-println(user.lastName)
- 
-
 }
